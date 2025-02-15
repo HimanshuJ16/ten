dbec965e4c8113bb1571fa5b6ffa20989a3075d8
unknown <himanshujangir16@gmail.com>
2025-02-15 18:10:31 +0530

    latest commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,23 +11,26 @@ interface DecodedToken {
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
-  const isPublicPath = path === '/auth/sign-in' || path === '/auth/sign-up'
-
+  const isPublicPath = path === '/' || path === '/auth/sign-in' || path === '/auth/sign-up'
+  
   const token = request.cookies.get('token')?.value || ''
 
-  if (isPublicPath && token) {
+  if (!token && !isPublicPath) {
+    return NextResponse.redirect(new URL('/auth/sign-in', request.url))
+  }
+
+  if (token) {
     try {
       const decodedToken = jwtDecode(token) as DecodedToken
       const { district, role, userId } = decodedToken
-      return NextResponse.redirect(new URL(`/${district}/${role}/${userId}/dashboard`, request.url))
+
+      // Redirect logged-in users from public pages to their dashboard
+      if (isPublicPath && path !== '/') {
+        return NextResponse.redirect(new URL(`/${district}/${role}/${userId}/dashboard`, request.url))
+      }
     } catch (error) {
       console.error('Error decoding token:', error)
-      // If there's an error decoding the token, we'll just continue to the public path
     }
-  }
-
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL('/auth/sign-in', request.nextUrl))
   }
 
   return NextResponse.next()
